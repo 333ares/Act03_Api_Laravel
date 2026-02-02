9a9a37ece119a601cda00d61489a5967678fd190
Ares <aresgp5335@gmail.com>
2026-02-02 12:48:12 +0100

    Ruta para ver a todos los animales
    He añadido la ruta GET para ver los animales existentes

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,5 @@
 use App\Http\Controllers\AnimalController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/animales', [AnimalController::class, 'index']);
 Route::post('/crearAnimal', [AnimalController::class, 'crearAnimal']);
